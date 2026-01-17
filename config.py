bf6d43a984d31d665d8a875e22804a0d96ca3ea1
financefuryy-creator <financefuryy@gmail.com>
2026-01-17 14:44:44 +0530

    Update config.py with Telegram bot details

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-# Configurations for Binance pairs bot system
-COINDCX_API_KEY = '42fbb3181d4edb1b6a4be1711fcf313906b5d0138cf8d3f5'
-COINDCX_SECRET_KEY = 'b64318aaacffdbd34ccd0c90fb17870fb5a6339006fac25a9bb4485364208fa2'
+# Telegram Bot Credentials
+TELEGRAM_BOT_TOKEN_1 = '8182721422:AAEiSsPsKlKB3reWGA5DvJTSulxPOAbNBrQ'
+TELEGRAM_CHAT_ID_1 = '7515010163'
 
-# Telegram credentials
-TELEGRAM_BOT_TOKEN = 'your_telegram_bot_token_here'
-TELEGRAM_CHAT_ID = 'your_chat_id_here'
+TELEGRAM_BOT_TOKEN_2 = '8557523608:AAHGJ_UOKEZaJc6iPfW0DITreuQ1Nx2mVv4'
+TELEGRAM_CHAT_ID_2 = '7650632969'
